08b73ef82dca1ce2afabd685132752df9a88a559
JoeyClementeCode <joewclemente@gmail.com>
2022-02-28 18:24:58 -0500

    nice

---
+++ Assets/Scripts/PeekMask.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace InverseWorld
+{
+    public class PeekMask : MonoBehaviour
+    {
+        private Tile currentTile;
+    }
+}
